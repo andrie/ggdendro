daf708fc74c9190e00605a6c2d2d746dee2b4599
Andrie <apdevries@gmail.com>
2013-09-02 15:38:29 +0100

    Minor mod to dendro_data.tree example

--- R/dendro_tree.R
+++ R/dendro_tree.R
@@ -47,7 +47,7 @@ tree_env <- new.env()
 #' tree_data <- dendro_data(cpus.ltr)
 #' ggplot(segment(tree_data)) +
 #' 	geom_segment(aes(x=x, y=y, xend=xend, yend=yend, size=n), 
-#' 		colour="blue", alpha=0.5) +
+#' 		colour="lightblue") +
 #' 	scale_size("n") +
 #' 	geom_text(data=label(tree_data), 
 #' 		aes(x=x, y=y, label=label), vjust=-0.5, size=4) +
